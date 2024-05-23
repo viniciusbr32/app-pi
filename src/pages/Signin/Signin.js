b412eca3c6dcd7c1ac333c63a4fe7a3641ce7adc
viniciusbr32 <viniciusgba2008@gmail.com>
2024-05-23 18:55:43 -0300

    Commit: Alteraçoes signin

--- src/pages/Signin/Signin.js
+++ src/pages/Signin/Signin.js
@@ -1,57 +1,69 @@
 import React, { useState } from 'react';
-
-import { Student } from '@phosphor-icons/react';
 import { Input } from '../../components/input/Input';
 import { Button } from '../../components/Button/Button';
-
 import * as C from './styles';
 import { Link, useNavigate } from 'react-router-dom';
-import { useAuth } from '../../hooks/useAuth';
 
 export const Signin = () => {
-	const { signin } = useAuth();
 	const navigate = useNavigate();
 
 	const [email, setEmail] = useState('');
-	const [senha, setSenha] = useState('');
+	const [password, setPassword] = useState('');
+	const [userType, setUserType] = useState('aluno');
 	const [error, setError] = useState('');
 
-	const handleLogin = () => {
-		if (!email | !senha) {
-			setError('Preencha todos os campos');
-			return;
+	const handleLogin = async () => {
+		try {
+			const response = await fetch('http://localhost:5000/usuarios');
+			if (!response.ok) {
+				throw new Error('Erro ao buscar usuários');
+			}
+
+			const usuarios = await response.json();
+
+			const usuario = usuarios.find(
+				(user) =>
+					user.email === email &&
+					user.senha === password &&
+					user.tipoUsuario === userType
+			);
+
+			if (usuario) {
+				localStorage.setItem('user', JSON.stringify(usuario));
+
+				navigate('/home');
+			} else {
+				setError('E-mail, senha ou tipo de usuário incorretos');
+			}
+		} catch (error) {
+			console.error('Erro ao fazer login:', error);
+			setError('Erro ao fazer login. Por favor, tente novamente mais tarde.');
 		}
-
-		const res = signin(email, senha);
-
-		if (res) {
-			setError(res);
-			return;
-		}
-
-		navigate('/home');
 	};
 
 	return (
 		<C.Container>
-			<C.Label>
-				{' '}
-				<Student size={32} />
-				LOGIN - PROJETO INTEGRADOR
-			</C.Label>
+			<C.Label>LOGIN - PROJETO INTEGRADOR</C.Label>
 			<C.Content>
 				<Input
 					type='email'
 					placeholder='Digite seu E-mail'
 					value={email}
-					onChange={(e) => [setEmail(e.target.value), setError('')]}
+					onChange={(e) => setEmail(e.target.value)}
 				/>
 				<Input
 					type='password'
 					placeholder='Digite sua Senha'
-					value={senha}
-					onChange={(e) => [setSenha(e.target.value), setError('')]}
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
 				/>
+				<C.Select
+					value={userType}
+					onChange={(e) => setUserType(e.target.value)}
+				>
+					<option value='aluno'>Aluno</option>
+					<option value='professor'>Professor</option>
+				</C.Select>
 				<C.labelError>{error}</C.labelError>
 				<Button text='Entrar' onClick={handleLogin} />
 
